3d6cfb5b2dc34a4b2729a3af2d54da8242ef402b
Züleyha Akbaş <zuleyhakbas04@gmail.com>
2025-08-08 12:22:36 +0300

    Apply SystemChrome style

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:drobee/presentation/splash/cubit/splash_cubit.dart';
 import 'package:drobee/presentation/splash/pages/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:drobee/data/services/remote_config_service.dart';
@@ -13,6 +14,9 @@ Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await RemoteConfigService.init();
   await RemoteConfigService.forceFetch();
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
   runApp(const MyApp());
 }
 
